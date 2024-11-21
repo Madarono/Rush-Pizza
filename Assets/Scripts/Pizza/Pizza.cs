eb0b13c962610565af7d247fc678129874c27852
Madarono <shadow.knoight@gmail.com>
2024-11-21 18:42:50 +0200

    Improved Pizza Script
    I improved on the pizza script to make it check the distance between toppings and check which side it is to ensure that the player is putting them correctly and not spamming them in random places.
    
    [Plans]
    I plant to add the furnace that GPGP uses and add its mechanics, though it may be hard as it is a 3D game and not a 2D game, but I pulled through this much so there is nothing stopping me now.

--- Assets/Scripts/Pizza/Pizza.cs
+++ Assets/Scripts/Pizza/Pizza.cs
@@ -7,36 +7,133 @@ public class Ingrediants
 {
     public string ingrediantName;
     public List<GameObject> ingrediantObj;
-    public float ingrediantPercentage;
     public float maxIngrediantPercentage = 100f;
+    public float ingrediantPercentage;
+
+    [Header("Side of the toppping")]
+    public List<GameObject> leftSideObj = new List<GameObject>();
+    public float leftPercentage;
+    public List<GameObject> rightSideObj = new List<GameObject>();
+    public float rightPercentage;
+
+    [Header("Separation of Toppings")]
+    public bool calculateDistance = true;
+    public List<float> distanceBetweenObjects = new List<float>();
+    public float averageDistance; 
 }
 
 public class Pizza : MonoBehaviour
 {
     public Ingrediants[] ingrediants;
+    private PlayerHolder playerHolder;
+    public GameObject sideSeparator; //Separates the left and right sides
+
+    void Start()
+    {
+        playerHolder = GameObject.Find("UniversalScripts").GetComponent<PlayerHolder>();
+    }
+
+    void Update()
+    {
+        if(playerHolder.drawing.topping != null)
+        {
+            sideSeparator.SetActive(true);
+        }
+        else
+        {
+            sideSeparator.SetActive(false);
+        }
+    }
 
     public void CheckName(string name, GameObject obj)
     {
-        for(int i = 0; i < ingrediants.Length; i++)
+        for (int i = 0; i < ingrediants.Length; i++)
         {
-            if(name == ingrediants[i].ingrediantName)
+            if (name == ingrediants[i].ingrediantName)
             {
+                string side = CheckSide(obj.transform.position);
+                if (side == "left")
+                {
+                    ingrediants[i].leftSideObj.Add(obj);
+                }
+                else if (side == "right")
+                {
+                    ingrediants[i].rightSideObj.Add(obj);
+                }
+
                 ingrediants[i].ingrediantObj.Add(obj);
+                UpdatePercentage(i);
+                if(ingrediants[i].calculateDistance)
+                {
+                    UpdateDistance(i);
+                }
                 break;
             }
         }
-        UpdatePercentage();
     }
 
-    public void UpdatePercentage()
+    public void UpdatePercentage(int id)
     {
-        for(int i = 0; i < ingrediants.Length; i++)
+        ingrediants[id].ingrediantPercentage = ingrediants[id].ingrediantObj.Count / ingrediants[id].maxIngrediantPercentage;
+        if (ingrediants[id].ingrediantPercentage > 1f)
         {
-            ingrediants[i].ingrediantPercentage = ingrediants[i].ingrediantObj.Count / ingrediants[i].maxIngrediantPercentage;
-            if(ingrediants[i].ingrediantPercentage > 1f)
-            {
-                ingrediants[i].ingrediantPercentage = 1f;
-            }
+            ingrediants[id].ingrediantPercentage = 1f;
         }
+
+        ingrediants[id].leftPercentage = ingrediants[id].leftSideObj.Count / (ingrediants[id].maxIngrediantPercentage / 2f);
+        if(ingrediants[id].leftPercentage > 1f)
+        {
+            ingrediants[id].leftPercentage = 1f;
+        }
+
+        ingrediants[id].rightPercentage = ingrediants[id].rightSideObj.Count / (ingrediants[id].maxIngrediantPercentage / 2f);
+        if(ingrediants[id].rightPercentage > 1f)
+        {
+            ingrediants[id].rightPercentage = 1f;
+        }
+    }
+
+    public void UpdateDistance(int id)
+    {
+        // If there's only one ingredient object, distances cannot be calculated
+        if (ingrediants[id].ingrediantObj.Count <= 1)
+        {
+            ingrediants[id].distanceBetweenObjects.Clear();
+            ingrediants[id].averageDistance = 0f;
+            return;
+        }
+
+        // Ensure the distanceBetweenObjects list matches the count of ingredient objects - 1
+        while (ingrediants[id].distanceBetweenObjects.Count < ingrediants[id].ingrediantObj.Count - 1)
+        {
+            ingrediants[id].distanceBetweenObjects.Add(0);
+        }
+        while (ingrediants[id].distanceBetweenObjects.Count > ingrediants[id].ingrediantObj.Count - 1)
+        {
+            ingrediants[id].distanceBetweenObjects.RemoveAt(ingrediants[id].distanceBetweenObjects.Count - 1);
+        }
+
+        float totalDistance = 0f;
+
+        for (int o = 0; o < ingrediants[id].ingrediantObj.Count - 1; o++)
+        {
+            // Calculate the distance between consecutive objects
+            float distance = Vector3.Distance(
+                ingrediants[id].ingrediantObj[o].transform.position,
+                ingrediants[id].ingrediantObj[o + 1].transform.position
+            );
+            ingrediants[id].distanceBetweenObjects[o] = distance;
+            totalDistance += distance;
+        }
+
+        // Calculate the average distance
+        ingrediants[id].averageDistance = totalDistance / (ingrediants[id].ingrediantObj.Count - 1);
+    }
+
+
+    private string CheckSide(Vector3 ingredientPosition)
+    {
+        Vector3 localPosition = transform.InverseTransformPoint(ingredientPosition);
+        return localPosition.x < 0 ? "left" : "right";
     }
 }
---
+++ Assets/Scripts/PlayerHolder.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHolder : MonoBehaviour
+{
+    public PlayerMovement movement;
+    public SauceDrawing drawing;
+}
